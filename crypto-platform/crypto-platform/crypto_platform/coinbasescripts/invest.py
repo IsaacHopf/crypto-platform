add9e575d67d7c73482f13977132415ef3260e2c
= <sdkauk@gmail.com>
2022-03-18 17:44:25 -0500

    the check_tax_loss_harvest function now checks the potential for harvesting any crypto bought on any date during the last year and saves the results in a new list

--- crypto-platform/crypto-platform/crypto_platform/coinbasescripts/invest.py
+++ crypto-platform/crypto-platform/crypto_platform/coinbasescripts/invest.py
@@ -11,6 +11,9 @@ from crypto_platform import views
 from datetime import date
 from datetime import timedelta
 
+all_crypto_prices = [['BTC', 0], ['ETH', 0], ['USDT', 0], ['LTC', 0]] #This list will contain every crypto we use and its price
+potential_harvest_dates = []
+
 possible_basket = [['BTC', 0.5], ['ETH', 0.5]] # I think the baskets are best represented as lists in the code. When we set up the database, we can put the data into a list like this.
 another_possible_basket = [['BTC', 0.4], ['ETH', 0.2], ['USDT', 0.2], ['YFI', 0.2]]
 
@@ -33,18 +36,24 @@ def make_initial_investment(user, basket, amount):
 def check_tax_loss_harvest(all_crypto_prices, user): # This is the function that we would run periodically to determine if we will send a notification to our users
     """Checks potential for tax-loss harvesting."""
     today = date.today()
+    threshold = -0.1
     print("Today is: ", today)
-    yesterday = today - timedelta(days = 1)
-    print("Yesterday was: ", yesterday)
 
-    for crypto_price in all_crypto_prices:
-        spot_price = get_spot_price(crypto_price[0]+'-USD', yesterday, user)
-        print (f"the price today is {crypto_price[1]}")
-        print (f"the spot price yesterday was {spot_price}")
-        change_in_price = float(crypto_price[1]) - float(spot_price["amount"]) 
-        print (f"the change in price was: {change_in_price}")
-        if (change_in_price < 0):
-            print ("there is potential for harvesting")
+    for crypto_price in all_crypto_prices: #loop through every currency
+        print (f"the current price today for {crypto_price[0]} is {crypto_price[1]}")
+        for i in range(365): #loop through the previous year
+            loop_day = today - timedelta(days = i)
+            spot_price = get_spot_price(crypto_price[0]+'-USD', loop_day, user)
+            amount = float(spot_price["amount"])
+            print (f"the spot price was {amount} on {loop_day}")
+            change_in_price = float(crypto_price[1]) - (amount) 
+            print (f"the change in price was: {change_in_price}")
+            if ((change_in_price/amount) <= threshold):
+                print ("there is potential for harvesting")
+                potential_harvest_dates.append([crypto_price[0], loop_day])
+    print ("............................................")
+    print ("These are the days with potential to harvest:")
+    print (potential_harvest_dates)
 
 def tax_loss_harvest(user): # This is the function that would run everytime the user activates it. Either by clicking a button, logging in, or withdrawing their investment.
     """Performs the tax-loss harvesting process."""
@@ -55,8 +64,6 @@ def tax_loss_harvest(user): # This is the function that would run everytime the 
     pass
 
 def get_all_crypto_prices(): # This is the function that will get the prices of every crypto currency we use
-
-    all_crypto_prices = [['BTC', 0], ['ETH', 0], ['USDT', 0], ['LTC', 0]] #This list will contain every crypto we use and its price.
 
     for crypto in all_crypto_prices: #Gets the price of every crypto by calling get_one_price for every crypto and saving the price.
         price = get_one_price(crypto[0])
@@ -73,11 +80,7 @@ def get_one_price(crypto): #This function will get the price of one crypto curre
 def get_spot_price(currency_pair, date, user): #currency_pair must be in format like 'BTC-USD' and date must be in format like '2022-3-2'
     spot_price = user.client.get_spot_price(currency_pair=currency_pair, date=date)
     return spot_price
-
-"""def get_one_price(crypto_prices, crypto):
-    for price in crypto_prices:
-        if (price[0] == crypto):
-            return price[1]"""
+        
 
 def process_investments(user):
     investing = True
