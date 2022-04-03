26f395ae822a3f2fab8b625b23aeff200ad36214
= <sdkauk@gmail.com>
2022-04-03 17:35:38 -0500

    wrote algorithm to find optimal amount to sell to maximize realized losses

--- crypto-platform/crypto-platform/crypto_platform/dashboard/invest.py
+++ crypto-platform/crypto-platform/crypto_platform/dashboard/invest.py
@@ -6,6 +6,8 @@ from flask import redirect, request
 import requests
 import json
 import time
+import datetime
+import re
 from crypto_platform.dashboard.User import User
 from datetime import date
 from datetime import timedelta
@@ -33,7 +35,10 @@ def make_initial_investment(user, basket, amount):
 
         user.buy_with_bank_payment_method(crypto, crypto_amount)
 
-def check_tax_loss_harvest(all_crypto_prices, user): # This is the function that we would run periodically to determine if we will send a notification to our users
+
+
+
+def predict_tax_loss_harvest(all_crypto_prices, user): # This is the function that we would run periodically to determine if we will send a notification to our users
     """Checks potential for tax-loss harvesting."""
     today = date.today()
     threshold = -0.1
@@ -55,16 +60,112 @@ def check_tax_loss_harvest(all_crypto_prices, user): # This is the function that
     print ("These are the days with potential to harvest:")
     print (potential_harvest_dates)
 
+
+
+
 def verify_tax_loss_harvest(user):
+
+    user_buys = get_user_buys(user)
+
+    #TEST DATA
+    """user_buys = [['BTC', '2022-03-16', '0.0002442', '41359.54'], 
+                 ['BTC', '2022-02-16', '0.0003442', '40359.54'], 
+                 ['BTC', '2022-01-16', '0.0002442', '39359.54'], 
+                 ['BTC', '2021-12-16', '0.0001442', '35359.54'],
+                 ['BTC', '2021-11-16', '0.0002442', '32359.54'],
+                 ['BTC', '2021-10-16', '0.0004442', '42359.54'],
+                 ['BTC', '2021-09-16', '0.0002442', '48359.54'],
+                 ['BTC', '2021-08-16', '0.0003442', '49359.54'],
+                 ['BTC', '2021-07-16', '0.0002442', '50359.54'],
+                 ['BTC', '2021-06-16', '0.0003442', '48359.54']]"""
+
+    btc_price = float(get_one_price('BTC'))
+    (total_btc_to_sell, net_losses) = check_harvest(user_buys, btc_price, user, 'BTC')
+
+    print (total_btc_to_sell, net_losses)
+
+def get_user_buys(user):
+
     all_buys = user.client.get_buys(user.client.get_account('BTC')['id'])
+
     for buy in all_buys["data"]:
 
         date = buy["created_at"]
-        amount = buy["amount"]
-        print (amount)
+        date = re.sub(r'T.*', '', date)
+        amount = str(buy["amount"])
+        amount = amount.split(' ')[-1]
         unit_price = buy["unit_price"]["amount"]
         user_buys.append(['BTC', date, amount, unit_price])
-    print (user_buys)
+
+    return user_buys
+
+
+def check_harvest(user_buys, current_price_of_coin, user, crypto):
+
+    threshold = 0.1
+    total_amount_of_coin = user.client.get_account(crypto)['balance']['amount']
+    #total_amount_of_coin = 0.002842 #TEST DATA
+    total_worth = current_price_of_coin * total_amount_of_coin
+    total_amount_in_buys = 0
+    shortened_buys = []
+
+    #THIS GOES THROUGH AND ELIMINATES ALL IRRELEVANT BUYS (buys that are too old)
+    for buy in user_buys:
+        total_amount_in_buys = total_amount_in_buys + float(buy[2])
+        if total_amount_in_buys > total_amount_of_coin:
+            break
+        else:
+            shortened_buys.append(buy)
+    user_buys = shortened_buys
+
+    #THIS SORTS THE BUYS FROM OLDEST TO NEWEST
+    user_buys = sorted(user_buys, key=lambda x: datetime.datetime.strptime(x[1], "%Y-%m-%d"))
+
+
+    #THIS PART FIGURES OUT WHICH BUY TO SELL TO OPTIMIZE REALIZED LOSSES
+    i=-1 #initialize iteration for the loop at -1 so that each iteration corresponds with list number
+    net_investment = 0
+    net_gains = 0
+    maximum_loss = 0
+    optimal_buy_to_harvest = 0
+    for buy in user_buys:
+        i=i+1;
+        change_in_price = (current_price_of_coin - float(buy[3]))
+        investment = (float(buy[2]) * float(buy[3]))
+        net_investment = net_investment + investment
+        gains = (current_price_of_coin * float(buy[2])) - (float(buy[3]) * float(buy[2]))
+        net_gains = net_gains + gains
+        buy.append(net_gains)
+        if (net_investment/net_gains) <= threshold:
+            buy.append(True)
+        else:
+            buy.append(False)
+        if (net_gains < maximum_loss):
+            maximum_loss = net_gains
+            optimal_buy_to_harvest = i
+
+    #user_buys =[crypto_id, yyyy-mm-dd, crypto_amount, unit_price, net_gains, above_threshold]
+
+    #THIS DETERMINES THE TOTAL AMOUNT OF COIN TO SELL
+    total_to_sell = 0
+    for buy in user_buys:
+        total_to_sell = total_to_sell + float(buy[2])
+        if float(buy[4]) <= maximum_loss:
+            break
+
+    #IF THE OPTIMAL_BUY_TO_HARVEST RETURNS FALSE, THEN SET TOTAL TO SELL to 0
+    if user_buys[optimal_buy_to_harvest][5] == False:     
+        total_to_sell = 0
+
+    for buy in user_buys:
+        print(buy)
+
+    return (total_to_sell, maximum_loss)
+
+    #user_buys = sorted(user_buys,key=lambda x:datetime.datetime.strptime(x[1],"%Y-%m-%d"))
+    #print (user_buys)
+
+
 
 def tax_loss_harvest(user): # This is the function that would run everytime the user activates it. Either by clicking a button, logging in, or withdrawing their investment.
     """Performs the tax-loss harvesting process."""
@@ -101,4 +202,4 @@ def process_investments(user):
     print (all_crypto_prices)
     verify_tax_loss_harvest(user)
     time.sleep(30)
-    check_tax_loss_harvest(all_crypto_prices, user)
+    predict_tax_loss_harvest(all_crypto_prices, user)
