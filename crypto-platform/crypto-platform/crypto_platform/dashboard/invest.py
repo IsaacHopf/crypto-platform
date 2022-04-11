e1630324c48b19932d8caaa265e93d96a029eb38
IsaacHopf <iplazma3@icloud.com>
2022-04-10 20:56:30 -0400

    Added make_withdrawal to invest.py to make withdrawals

--- crypto-platform/crypto-platform/crypto_platform/dashboard/invest.py
+++ crypto-platform/crypto-platform/crypto_platform/dashboard/invest.py
@@ -3,10 +3,11 @@ Scripts for investing and withdrawing.
 """
 from flask import flash
 from crypto_platform.dashboard.User import User
+import time
 
-def make_initial_investment(user, basket, amount):
+def make_investment(user, basket, invested_amount):
     """
-    Makes the user's initial investment for a selected basket.
+    Makes the user's investment for a selected basket.
 
     user: the user, represented as an object of the User class
     basket: the user's selected basket, represented as a list of lists of crypto percentages that add up to 1 (Ex. [['BTC', 0.5], ['ETH', 0.5]])
@@ -15,8 +16,27 @@ def make_initial_investment(user, basket, amount):
     for crypto_percentage in basket:
         crypto = crypto_percentage[0] # The cryptocurrency.
         percent = crypto_percentage[1] # The percentage this cryptocurrency makes up in the basket.
-        crypto_amount = amount * percent # The amount of this cryptocurrency to buy.
+        buy_amount = invested_amount * percent # The amount of this cryptocurrency to buy.
 
-        #user.test_buy(crypto, crypto_amount)
-        user.buy_with_bank_payment_method(crypto, crypto_amount)
+        #user.test_buy(crypto, buy_amount)
+        user.buy_with_bank_payment_method(crypto, buy_amount)
     flash('Your investment has been processed! You should recieve several emails from Coinbase.')
+
+def make_withdrawal(user, basket, invested_amount):
+    """
+    Makes the user's withdrawal for a selected basket.
+
+    user: the user, represented as an object of the User class
+    basket: the user's selected basket, represented as a list of lists of crypto percentages that add up to 1 (Ex. [['BTC', 0.5], ['ETH', 0.5]])
+    invested_amount: the amount to invest for the selected basket (in the user's native currency)
+    """
+    for crypto_percentage in basket:
+        crypto = crypto_percentage[0] # The cryptocurrency.
+        percent = crypto_percentage[1] # The percentage this cryptocurrency makes up in the basket.
+        sell_amount = invested_amount * percent # The amount of this cryptocurrency to sell.
+
+        user.sell(crypto, sell_amount)
+
+    time.sleep(60) # Wait for a minute (NOT A SECURE SOLUTION)
+    user.withdraw(user.get_cash_wallet_balance())
+    flash('Your withdrawal has been processed! You should recieve several emails from Coinbase.')
